d4dc51d6af41030e553ce013ba5088a1e763596a
Vladislav Illarionov <vladislavillarionov32@gmail.com>
2019-11-29 02:39:20 +0300

    fix

--- messenger/chats/models.py
+++ messenger/chats/models.py
@@ -5,11 +5,15 @@ from users.models import User
 
 class Chat(models.Model):
     is_group_chat = models.BooleanField(verbose_name='Групповой ли чат')
-    topic = models.CharField(max_length=60,
-                            verbose_name='Название')
-    last_message = models.TextField(max_length=100,
-                                    verbose_name='Последнее сообщение',
-                                    null=True)
+    topic = models.CharField(
+        max_length=60,
+        verbose_name='Название',
+    )
+    last_message = models.TextField(
+        max_length=100,
+        verbose_name='Последнее сообщение',
+        null=True,
+    )
 
     def __str__(self):
         return self.topic
@@ -20,17 +24,22 @@ class Chat(models.Model):
 
 
 class Message(models.Model):
-    chat = models.ForeignKey(to=Chat,
-                            on_delete=models.CASCADE,
-                            verbose_name='Чат')
-    user = models.ForeignKey(to=User,
-                            on_delete=models.CASCADE,
-                            verbose_name='Пользователь')
+    chat = models.ForeignKey(
+        to=Chat,
+        on_delete=models.CASCADE,
+        verbose_name='Чат',
+    )
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+    )
     content = models.TextField(verbose_name='Содержание')
-    added_at = models.DateTimeField(null=True,
-                                    # default=timezone.now(),
-                                    auto_now_add=True,
-                                    verbose_name='Дата отправки')
+    added_at = models.DateTimeField(
+        null=True,
+        auto_now_add=True,
+        verbose_name='Дата отправки',
+    )
 
     class Meta:
         verbose_name = 'Сообщение'
@@ -39,19 +48,29 @@ class Message(models.Model):
 
 
 class Attachment(models.Model):
-    chat = models.ForeignKey(to=Chat,
-                            on_delete=models.CASCADE,
-                            verbose_name='Чат')
-    user = models.ForeignKey(to=User,
-                            on_delete=models.CASCADE,
-                            verbose_name='Пользователь')
-    message = models.ForeignKey(to=Message,
-                                on_delete=models.CASCADE,
-                                verbose_name='Сообщение')
-    attach_type = models.TextField(max_length=60,
-                                verbose_name='Тип вложения')
-    url = models.CharField(max_length=100,
-                        verbose_name='Ссылка')
+    chat = models.ForeignKey(
+        to=Chat,
+        on_delete=models.CASCADE,
+        verbose_name='Чат',
+    )
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+    )
+    message = models.ForeignKey(
+        to=Message,
+        on_delete=models.CASCADE,
+        verbose_name='Сообщение',
+    )
+    attach_type = models.TextField(
+        max_length=60,
+        verbose_name='Тип вложения',
+    )
+    url = models.CharField(
+        max_length=100,
+        verbose_name='Ссылка',
+    )
 
     class Meta:
         verbose_name = 'Вложение'
@@ -59,17 +78,23 @@ class Attachment(models.Model):
 
 
 class Member(models.Model):
-    user = models.ForeignKey(to=User,
-                            on_delete=models.CASCADE,
-                            verbose_name='Пользователь')
-    chat = models.ForeignKey(to=Chat,
-                            on_delete=models.CASCADE,
-                            verbose_name='Чат')
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+    )
+    chat = models.ForeignKey(
+        to=Chat,
+        on_delete=models.CASCADE,
+        verbose_name='Чат',
+    )
     new_messages = models.IntegerField(verbose_name='Кол-во новых сообщений')
-    last_read_message = models.ForeignKey(to=Message,
-                                        null=True,
-                                        on_delete=models.CASCADE,
-                                        verbose_name='Последнее прочитанное')
+    last_read_message = models.ForeignKey(
+        to=Message,
+        null=True,
+        on_delete=models.CASCADE,
+        verbose_name='Последнее прочитанное',
+    )
 
     class Meta:
         verbose_name = 'Участник'
